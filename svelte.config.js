1adfc6651adc4a8d24fe3be64fd85dfac89a392e
loganfritz <lgfritz@asu.edu>
2024-12-19 11:42:11 -0700

    added ;

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 // import adapter from '@sveltejs/adapter-auto';
-import adapter from 'amplify-adapter'
+import adapter from 'amplify-adapter';
 // import adapter from 'amplify-adapter';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
